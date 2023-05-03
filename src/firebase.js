4bcc83b31e5b44e120afad03cc90d0e093d686d5
Horatiu <horatiu.oltn@gmail.com>
2023-05-03 14:09:39 +0300

    .env fix

--- src/firebase.js
+++ src/firebase.js
@@ -3,19 +3,20 @@ import { initializeApp } from 'firebase/app';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from 'firebase/auth';
-import {getFirestore} from 'firebase/firestore'
+import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyC440WXdRwlj7IOrrZG63IEHSmj6n59Pfk',
-  authDomain: 'react-chat-f7a81.firebaseapp.com',
-  projectId: 'react-chat-f7a81',
-  storageBucket: 'react-chat-f7a81.appspot.com',
-  messagingSenderId: '47003997300',
-  appId: '1:47003997300:web:bb24166e7400936ce52d21',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const auth = getAuth(app);
+export const db = getFirestore(app);
